a39cfb365b7b1d7c84e69eb3ee1e34bc1304766b
Michael Hoyle <mcalebhoyle@gmail.com>
2022-03-02 20:26:00 -0500

    Fixed issue where questions wouldn't display

--- script.js
+++ script.js
@@ -1,88 +1,82 @@
 var startEl = document.querySelector("#startPage");
 var gameEl = document.querySelector("#gamePage");
 var endEl = document.querySelector("#endPage");
+var questionsEl = document.querySelector("#questions");
+
 var startBtn = document.querySelector("#startBtn");
 var initialsInput = document.querySelector("#initials");
-var questionsEl = document.querySelector("#question");
-var answerBtns = document.querySelector("#answer-buttons");
-var nextBtn = document.querySelector("#next-btn");
-var cursor = 0;
-var score = startTimer();
 
-var shuffledQuestions, currentQuestionIndex;
+var questionIndex = 0;
 
-startBtn.addEventListener("click", game);
+var titleEl = document.querySelector("#title");
+
+function renderQuestion() {
+  var question = questions[questionIndex];
+  var currentQuestion = question.question;
+  titleEl.textContent = currentQuestion;
+  questionsEl.innerHTML = '';
+
+  for (var i = 0; i < question.possible.length; i++) {
+    var item = question.possible[i];
+    var answerBtn = document.createElement("button");
+    // node.dataset.value = key;
+    answerBtn.textContent = i + 1 + ". " + item;
+    questionsEl.appendChild(answerBtn);
+  }
+}
 
 var questions = [
   {
-    question:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-    answers: [
-      { text: "4", correct: true },
-      { text: "22", correct: false },
-    ],
+    question: "Lorem ipsum dolor",
+    possible: ["curly brackets", "parentheses", "block", "example"],
+    correct: 2,
+  },
+  {
+    question: "Lorem ipsum dolor sit",
+    possible: ["5", "6", "7", "8"],
+    correct: 3,
+  },
+  {
+    question: "Lorem ipsum dolor sit amet",
+    possible: ["9", "10", "11", "12"],
+    correct: 0,
   },
 ];
 
-function start() {
+function startScreen() {
   startEl.style.display = "block";
   gameEl.style.display = "none";
   endEl.style.display = "none";
 }
-function game() {
+
+function gameScreen() {
   startEl.style.display = "none";
   gameEl.style.display = "block";
   endEl.style.display = "none";
-  shuffledQuestions = questions.sort(() => Math.random() - 0.5);
-  currentQuestionIndex = 0;
-//   startTimer();
-  setNextQuestion();
+  renderQuestion();
 }
 
-function setNextQuestion() {
-  resetState();
-  showQuestion(shuffledQuestions[currentQuestionIndex]);
-}
-
-function showQuestion(question) {
-  questionsEl.innerText = question.question;
-  question.answers.forEach((answer) => {
-    const button = document.createElement("button");
-    button.innerText = answer.text;
-    button.classList.add("btn");
-    if (answer.correct) {
-      button.dataset.correct = answer.correct;
-    }
-    button.addEventListener("click", selectAnswer);
-    answerBtns.appendChild(button);
-  });
-}
-
-function resetState() {
-  nextBtn.classList.add("hide");
-  while (answerBtns.firstChild) {
-    answerBtns.removeChild(answerBtns.firstChild);
-  }
-}
-
-function selectAnswer() {}
-
-function startTimer() {
-  var sec = 75;
-  var timer = setInterval(function () {
-    document.getElementById("timerDisplay").innerHTML = sec;
-    sec--;
-    if (sec < 0) {
-      clearInterval(timer);
-    }
-  }, 1000);
-  return sec;
-}
-
-function end() {
+function endScreen() {
   startEl.style.display = "none";
   gameEl.style.display = "none";
   endEl.style.display = "block";
 }
 
-start();
+function init() {
+  startScreen();
+}
+
+startBtn.addEventListener("click", gameScreen);
+gameEl.addEventListener("click", function (event) {
+  if (event.target.matches("button")) {
+    console.log(event.target);
+    questionIndex++;
+    if (questionIndex < questions.length) {
+      renderQuestion();
+    } else {
+      endScreen();
+    }
+  }
+});
+
+init();
